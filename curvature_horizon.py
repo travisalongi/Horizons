2e60208f83ed78b40b27bd5685e19db548f25ed3
travisalongi <travis.alongi@gmail.com>
2021-10-12 15:28:39 -0700

    update

--- curvature_horizon.py
+++ curvature_horizon.py
@@ -1,7 +1,5 @@
 """
 Want to see dominant wavelength and see if correspond to width of damage zone
-I think there are still problems in this code
-== This is probably not the way I want to proceed ==
 
 author: talongi
 date: sept. 2021
@@ -14,11 +12,12 @@ import matplotlib.pyplot as plt
 from obspy import Trace
 from obspy.signal.util import smooth
 from scipy.interpolate import UnivariateSpline
+plt.ioff()
 
 
 #%% Proof of concept -- use spline interpolation to compute 2nd derv.
 x = np.linspace(0,10,1000)
-y = x**3
+y = np.sin(x)
 y_spl = UnivariateSpline(x, y, s = 0)
 
 
@@ -30,7 +29,7 @@ plt.plot(x, y_spl.derivative(n=2)(x), 'r')
 
 
 # === Import - Horizons ===
-df = pd.read_csv('all_inversion+.dat', sep = '\s+',
+df = pd.read_csv('all_inversion+2.dat', sep = '\s+',
                  names = ['horizons','x','y','z'])
 horizon_names = df.horizons.unique()
 
@@ -39,7 +38,9 @@ dfs = dict(tuple(df.groupby('horizons')))
 print('Horizons Loaded...'); [print(k) for k in dfs.keys()]
 del df # attempt to keep memory use low
 
+#%%
 # === Loop through horizons ===
+xlims = [389000, 403100]
 for df in dfs:
     # Select data for horizon from data frame
     hor = dfs[df]
@@ -47,44 +48,66 @@ for df in dfs:
 
     # Find where jumps in x are these are x-lines
     w = np.where(np.diff(x) < 10)[0]
+    
     # Create colors for plot
     colors = plt.cm.viridis(np.linspace(0, 1, len(w)))
 
     plt.close('all')
-    f,(ax,ax2,ax3) = plt.subplots(nrows = 3, figsize = [10,10])
+    f,(ax,ax2,ax3,ax4) = plt.subplots(nrows = 4, figsize = [10,15])
+    f2,(axm) = plt.subplots(nrows = 1, figsize = [10,10])
     # Loop through the x-lines
+    ms = []
     for i, (low, high) in enumerate(zip(w[:-1], w[1:])):
         # Mask
-        m = np.arange(low + 1,high) + 1
+        m = np.arange(low + 1,high) + 1;ms.append(len(m))
+        # print(len(m))
+        if (np.floor(np.unique(np.diff(x[m]))) != 13).any():
+            print(i)
 
-        # Select longer profiles & and east of the noise
-        if (x[m].max() - x[m].min() > 500) and (x[m].min() > 389800):
-
+        # Select longer profiles & and north of the noise
+        if (x[m].max() - x[m].min() > 2000) and  (y[m].min() >3709700):
+            
+            # Map view plot
+            axm.plot(x[m], y[m], color = colors[i], alpha = 0.5)
+            
             # Preprocess twt data & use obspy Trace  
             # z_proc = Trace(z[m]).detrend().taper(0.15)
             z_proc = Trace(z[m])
-            n_smooth = 30
+            n_smooth = 10
             dx = np.median(np.diff(x))
             sm = smooth(z_proc, n_smooth)
 
             # Use spline interpolation and take second derivative 
             spl = UnivariateSpline(x[m], sm, s = 0)
             z_dd = spl.derivative(n=1)(x[m])
+            z_dd2 = spl.derivative(n=2)(x[m])
 
             # Start plots
             ax.plot(x[m], z_proc, '-', color = colors[i], alpha = 0.15)
             ax2.plot(x[m], sm, '-', color = colors[i], alpha = 0.15)
             ax3.plot(x[m], z_dd, '-', color = colors[i], alpha = 0.15)
+            ax4.plot(x[m], z_dd2, '-', color = colors[i], alpha = 0.15)
 
             ax.invert_yaxis() 
             ax2.invert_yaxis() 
             ax3.invert_yaxis() 
+            ax4.invert_yaxis() 
             
+            ax.set_xlim(xlims)
+            ax2.set_xlim(xlims)
+            ax3.set_xlim(xlims)
+            ax4.set_xlim(xlims)
+
+            ax.xaxis.set_ticklabels([])
+            ax2.xaxis.set_ticklabels([])
+            ax3.xaxis.set_ticklabels([])
+
             ax.set_ylabel('twt [ms]')
             ax2.set_ylabel('twt [ms]')
-            ax3.set_ylabel('Second derivative')
+            ax3.set_ylabel('First Derivative')
+            ax4.set_ylabel('Second Derivative')
             
-            ax3.set_xlabel('Easting [m]')
+            ax4.set_xlabel('Easting [m]')
             
             ax.set_title('{} Raw'.format(df.split('_')[:-1][0].capitalize()),
                     fontsize = 28)
@@ -92,5 +115,7 @@ for df in dfs:
                 dx * n_smooth,
                 fontsize = 28))
             ax3.set_title('Derivative of Smoothed Data')
+            ax4.set_title('Derivative of Smoothed Data')
 
     f.savefig('Curvature_figs/{}.png'.format(str(df)))
+    f2.savefig('Curvature_figs/map_{}.png'.format(str(df)))
--- fourier_horizon.py
+++ fourier_horizon.py
@@ -4,6 +4,7 @@ Do fourier analysis on horizons
 Want to see dominant wavelength and see if correspond to width of damage zone
 I think there are still problems in this code
 == This is probably not the way I want to proceed ==
+== WIP and probably broken ==
 
 author: talongi
 date: sept. 2021
@@ -15,11 +16,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from obspy import Trace
 from obspy.signal.filter import bandpass
-from scipy.signal import detrend
+from scipy.signal import detrend, welch
 
 
 # === Import - Horizons ===
-df = pd.read_csv('all_inversion+.dat', sep = '\s+',
+df = pd.read_csv('all_inversion+2.dat', sep = '\s+',
                  names = ['horizons','x','y','z'])
 horizon_names = df.horizons.unique()
 # Load unique horizons into dictionary
@@ -64,9 +65,9 @@ for df in dfs:
 
 
             Z = np.abs(np.fft.fft(z_proc))
-            PSD = Z ** 2 * dx / N / dx
-            z_list.append(Z)
-            psd_list.append(PSD)
+            # PSD = Z ** 2 * dx / N / dx
+            z_list.append(z_proc.data)
+            # psd_list.append(PSD)
 
             # Make plot of spatial data
             ax.plot(x[m], z_proc, '-', color = colors[i], alpha = 0.15)
@@ -77,6 +78,7 @@ for df in dfs:
             # Make plot of frequency data
             axf.plot(1/freq[1:nyquist], Z[1:nyquist], 
                     '-', color = colors[i], alpha = 0.15)
+            axf.grid('all')
     axf.set_title(df.split('_')[:-1][0].capitalize())
     f.savefig('Fourier_figs/obspy_{}.png'.format(str(df)))
     ff.savefig('Fourier_figs/freq_{}.png'.format(str(df)))
@@ -91,10 +93,51 @@ for df in dfs:
         zd[k,inds] = list
 
     avg = np.nanmean(zd, axis = 0)
+    
+    # Sampling Rate
+    fs = np.abs(1 / np.median(np.diff(x)))
+    # PSD calc
+    f, Pxx = welch(avg, fs=fs, window='hann', nfft=None, 
+                   return_onesided=True, scaling='density', axis=-1, average='mean')
+    
+    
     ffs, axfs = plt.subplots(figsize = [10,10])
-    axfs.plot(avg, '-ok')
+    axfs.loglog(1/f, Pxx, '-ok')
     axfs.set_title(df.split('_')[:-1][0].capitalize())
-    ffs.savefig('Fourier_figs/stack_{}.png'.format(str(df)))
+    ffs.savefig('Fourier_figs/PDS_{}.png'.format(str(df)))
     
     
 
+#%%
+# Try welch poser spectral
+fs = np.abs(1/np.median(np.diff(x)))
+
+f, Pxx = welch(avg, fs=fs, window='hann', nfft=None, 
+                          return_onesided=True, scaling='density', axis=-1, average='mean')
+
+plt.figure(figsize = (10,5))
+plt.loglog(1/f,Pxx)
+# plt.xlim([0,5000])
+plt.ylabel('Power Spectral Density ($m^2/s$)') #units are [x]^2[dt]
+_ = plt.xlabel('Frequency')
+
+
+#%%
+rng = np.random.default_rng()
+fs = 10e3
+N = 1e6
+amp = 2 * np.sqrt(2)
+freq = 1234
+time = np.arange(N) / fs
+noise_power = 0.001 * fs / 2
+x = amp * np.sin(2*np.pi * freq * time)
+x += rng.normal(scale=np.sqrt(noise_power), size=time.shape)
+
+plt.figure()
+plt.plot(time,x)
+
+f, Pxx = welch(x, 1/fs, nperseg = 1024, 
+               window='hann', noverlap=None, nfft=None, detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
+plt.figure()
+plt.semilogy(f,Pxx)
+# plt.ylim([0.5e-3,1])
